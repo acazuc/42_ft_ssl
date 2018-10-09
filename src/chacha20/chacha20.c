572f848f035d0058b4138b376d9f5ab534fee5a8
acazuc <acazuc@student.42.fr>
2018-10-09 11:14:58 +0200

    More norming

--- src/chacha20/chacha20.c
+++ src/chacha20/chacha20.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/12 11:08:55 by acazuc            #+#    #+#             */
-/*   Updated: 2018/10/08 13:01:33 by acazuc           ###   ########.fr       */
+/*   Updated: 2018/10/09 11:14:44 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,22 @@
 
 void		chacha20_init_state(t_chacha20_ctx *ctx, uint8_t *key, uint8_t *iv)
 {
-	ctx->state[0] = ((uint32_t)'e') | ((uint32_t)'x' << 8)
-		| ((uint32_t)'p' << 16) | ((uint32_t)'a' << 24);
-	ctx->state[1] = ((uint32_t)'n') | ((uint32_t)'d' << 8)
-		| ((uint32_t)' ' << 16) | ((uint32_t)'3' << 24);
-	ctx->state[2] = ((uint32_t)'2') | ((uint32_t)'-' << 8)
-		| ((uint32_t)'b' << 16) | ((uint32_t)'y' << 24);
-	ctx->state[3] = ((uint32_t)'t') | ((uint32_t)'e' << 8)
-		| ((uint32_t)' ' << 16) | ((uint32_t)'k' << 24);
-	ctx->state[4] = *(uint32_t*)key;
-	ctx->state[5] = *(uint32_t*)(key + 4);
-	ctx->state[6] = *(uint32_t*)(key + 8);
-	ctx->state[7] = *(uint32_t*)(key + 12);
-	ctx->state[8] = *(uint32_t*)(key + 16);
-	ctx->state[9] = *(uint32_t*)(key + 20);
-	ctx->state[10] = *(uint32_t*)(key + 24);
-	ctx->state[11] = *(uint32_t*)(key + 28);
-	ctx->state[12] = *(uint32_t*)iv;
-	ctx->state[13] = *(uint32_t*)(iv + 4);
-	ctx->state[14] = *(uint32_t*)(iv + 8);
-	ctx->state[15] = *(uint32_t*)(iv + 12);
+	ctx->state[0x0] = 0x65787061;
+	ctx->state[0x1] = 0x6E642051;
+	ctx->state[0x2] = 0x322D6279;
+	ctx->state[0x3] = 0x7465206b;
+	ctx->state[0x4] = ((uint32_t*)key)[0];
+	ctx->state[0x5] = ((uint32_t*)key)[1];
+	ctx->state[0x6] = ((uint32_t*)key)[2];
+	ctx->state[0x7] = ((uint32_t*)key)[3];
+	ctx->state[0x8] = ((uint32_t*)key)[4];
+	ctx->state[0x9] = ((uint32_t*)key)[5];
+	ctx->state[0xA] = ((uint32_t*)key)[6];
+	ctx->state[0xB] = ((uint32_t*)key)[7];
+	ctx->state[0xC] = ((uint32_t*)iv)[0];
+	ctx->state[0xD] = ((uint32_t*)iv)[1];
+	ctx->state[0xE] = ((uint32_t*)iv)[2];
+	ctx->state[0xF] = ((uint32_t*)iv)[3];
 }
 
 static void	chacha20_round(uint32_t *a, uint32_t *b, uint32_t *c, uint32_t *d)
@@ -49,6 +45,18 @@ static void	chacha20_round(uint32_t *a, uint32_t *b, uint32_t *c, uint32_t *d)
 	*b = rotate_left32(*b ^ *c, 7);
 }
 
+static void	chacha20_rounds(uint32_t *x)
+{
+	chacha20_round(&x[0], &x[4], &x[8], &x[12]);
+	chacha20_round(&x[1], &x[5], &x[9], &x[13]);
+	chacha20_round(&x[2], &x[6], &x[10], &x[14]);
+	chacha20_round(&x[3], &x[7], &x[11], &x[15]);
+	chacha20_round(&x[0], &x[5], &x[10], &x[15]);
+	chacha20_round(&x[1], &x[6], &x[11], &x[12]);
+	chacha20_round(&x[2], &x[7], &x[8], &x[13]);
+	chacha20_round(&x[3], &x[4], &x[9], &x[14]);
+}
+
 void		chacha20_operate_block(t_chacha20_ctx *ctx, uint8_t *block)
 {
 	uint32_t	tmp[16];
@@ -60,14 +68,7 @@ void		chacha20_operate_block(t_chacha20_ctx *ctx, uint8_t *block)
 	i = 0;
 	while (i < 10)
 	{
-		chacha20_round(&x[0], &x[4], &x[8], &x[12]);
-		chacha20_round(&x[1], &x[5], &x[9], &x[13]);
-		chacha20_round(&x[2], &x[6], &x[10], &x[14]);
-		chacha20_round(&x[3], &x[7], &x[11], &x[15]);
-		chacha20_round(&x[0], &x[5], &x[10], &x[15]);
-		chacha20_round(&x[1], &x[6], &x[11], &x[12]);
-		chacha20_round(&x[2], &x[7], &x[8], &x[13]);
-		chacha20_round(&x[3], &x[4], &x[9], &x[14]);
+		chacha20_rounds(x);
 		++i;
 	}
 	i = 0;
