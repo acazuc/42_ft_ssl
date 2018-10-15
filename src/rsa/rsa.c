276bf955782e926c6836fcbe7548206d9c0e3dcd
acazuc <acazuc@student.42.fr>
2018-10-15 14:39:55 +0200

    Little osef changes

--- src/rsa/rsa.c
+++ src/rsa/rsa.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/04 14:37:29 by acazuc            #+#    #+#             */
-/*   Updated: 2018/10/12 14:43:32 by acazuc           ###   ########.fr       */
+/*   Updated: 2018/10/15 14:39:12 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,13 @@ int	rsa_dec(t_rsa_ctx *ctx, t_bignum *m, t_bignum *c)
 {
 	t_bignum	m1;
 	t_bignum	m2;
+	int			ret;
 
 	if (c->sign || bignum_cmp(c, ctx->n) >= 0)
 		return (0);
 	bignum_init(&m1);
 	bignum_init(&m2);
+	ret = 1;
 	if (!bignum_mod_exp(&m1, c, ctx->dmp, ctx->p)
 			|| !bignum_mod_exp(&m2, c, ctx->dmq, ctx->q)
 			|| !bignum_sub(&m1, &m1, &m2)
@@ -36,14 +38,10 @@ int	rsa_dec(t_rsa_ctx *ctx, t_bignum *m, t_bignum *c)
 			|| !bignum_mod(&m1, &m1, ctx->p)
 			|| !bignum_mul(&m1, &m1, ctx->q)
 			|| !bignum_add(&m1, &m1, &m2))
-	{
-		bignum_clear(&m1);
-		bignum_clear(&m2);
-		return (0);
-	}
+		ret = 0;
+	bignum_clear(&m1);
 	bignum_clear(&m2);
-	bignum_move(m, &m1);
-	return (1);
+	return (ret);
 }
 
 int	rsa_sign(t_rsa_ctx *ctx, t_bignum *s, t_bignum *m)
