f89925c429a01a818e62129621bee985b29d8e52
acazuc <acazuc@student.42.fr>
2019-08-04 20:58:41 +0200

    Major improvement of bignum_lshift & bignum_rshift (real shifting instead of n * 2 ^ n)

--- src/bignum/bignum_lshift_op.c
+++ src/bignum/bignum_lshift_op.c
@@ -6,51 +6,80 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/08 17:54:57 by acazuc            #+#    #+#             */
-/*   Updated: 2018/10/08 13:52:30 by acazuc           ###   ########.fr       */
+/*   Updated: 2019/08/04 20:50:43 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bignum.h"
 
-static int	do_clear(t_bignum *tmp1, t_bignum *tmp2, t_bignum *tmp3)
+static int		test_simple(t_bignum *r, t_bignum *a, uint32_t n)
 {
-	bignum_clear(tmp1);
-	bignum_clear(tmp2);
-	bignum_clear(tmp3);
-	return (0);
-}
-
-static int	do_init(t_bignum *tmp1, t_bignum *tmp2, t_bignum *tmp3)
-{
-	bignum_init(tmp1);
-	bignum_init(tmp2);
-	bignum_init(tmp3);
-	if (!bignum_grow(tmp1, 2))
-		return (0);
-	return (1);
-}
-
-int			bignum_lshift_op(t_bignum *r, t_bignum *a, uint32_t n)
-{
-	t_bignum	tmp1;
-	t_bignum	tmp2;
-	t_bignum	tmp3;
-
 	bignum_trunc(a);
+	if (!n)
+	{
+		bignum_copy(r, a);
+		return (1);
+	}
 	if (bignum_is_zero(a))
 	{
 		bignum_zero(r);
 		return (1);
 	}
-	if (!do_init(&tmp1, &tmp2, &tmp3))
-		return (do_clear(&tmp1, &tmp2, &tmp3));
-	if (!bignum_grow(&tmp2, n))
-		return (do_clear(&tmp1, &tmp2, &tmp3));
-	if (!bignum_exp(&tmp3, &tmp1, &tmp2))
-		return (do_clear(&tmp1, &tmp2, &tmp3));
-	if (!bignum_mul(r, a, &tmp3))
-		return (do_clear(&tmp1, &tmp2, &tmp3));
-	do_clear(&tmp1, &tmp2, &tmp3);
-	bignum_trunc(r);
+	return (0);
+}
+
+static int		init(t_bignum *tmp, t_bignum *a, uint32_t n, uint32_t *s8)
+{
+	uint32_t	len;
+
+	*s8 = sizeof(*tmp->data) * 8;
+	len = (bignum_num_bits(a) + n + *s8 - 1) / *s8;
+	bignum_init(tmp);
+	if (!bignum_reserve(tmp, len))
+		return (0);
+	tmp->len = len;
+	return (1);
+}
+
+static void		do_shift(t_bignum_word *tmp, t_bignum *a, int32_t *next, uint32_t s8)
+{
+	int32_t	nms8;
+
+	if (*next > 0)
+	{
+		nms8 = *next % s8;
+		*tmp = (a->data[*next / s8]
+				& ((t_bignum_word)(-1) << nms8)) >> nms8;
+	}
+	else
+		*tmp = 0;
+	if ((*next += s8) >= (int32_t)a->len * (int32_t)s8)
+		return ;
+	if (*next > 0)
+	{
+		nms8 = *next % s8;
+		*tmp |= (a->data[*next / s8]
+				& ((t_bignum_word)(-1) >> (s8 - nms8))) << (s8 - nms8);
+	}
+}
+
+int			bignum_lshift_op(t_bignum *r, t_bignum *a, uint32_t n)
+{
+	t_bignum	tmp;
+	int32_t		next;
+	uint32_t	i;
+	uint32_t	s8;
+
+	if (test_simple(r, a, n))
+		return (1);
+	if (!init(&tmp, a, n, &s8))
+		return (0);
+	i = -1;
+	while (++i < tmp.len)
+	{
+		next = i * s8 - n;
+		do_shift(&tmp.data[i], a, &next, s8);
+	}
+	bignum_move(r, &tmp);
 	return (1);
 }
--- src/bignum/bignum_rshift_op.c
+++ src/bignum/bignum_rshift_op.c
@@ -6,51 +6,72 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/08 17:56:46 by acazuc            #+#    #+#             */
-/*   Updated: 2018/10/08 13:55:20 by acazuc           ###   ########.fr       */
+/*   Updated: 2019/08/04 20:42:39 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bignum.h"
 
-static int	do_clear(t_bignum *tmp1, t_bignum *tmp2, t_bignum *tmp3)
+static int		test_simple(t_bignum *r, t_bignum *a, uint32_t n)
 {
-	bignum_clear(tmp1);
-	bignum_clear(tmp2);
-	bignum_clear(tmp3);
-	return (0);
-}
-
-static int	do_init(t_bignum *tmp1, t_bignum *tmp2, t_bignum *tmp3)
-{
-	bignum_init(tmp1);
-	bignum_init(tmp2);
-	bignum_init(tmp3);
-	if (!bignum_grow(tmp1, 2))
-		return (0);
-	return (1);
-}
-
-int			bignum_rshift_op(t_bignum *r, t_bignum *a, uint32_t n)
-{
-	t_bignum	tmp1;
-	t_bignum	tmp2;
-	t_bignum	tmp3;
-
 	bignum_trunc(a);
-	if (bignum_is_zero(a))
+	if (!n)
+	{
+		bignum_copy(r, a);
+		return (1);
+	}
+	if (bignum_is_zero(a) || n >= (uint32_t)bignum_num_bits(a))
 	{
 		bignum_zero(r);
 		return (1);
 	}
-	if (!do_init(&tmp1, &tmp2, &tmp3))
-		return (do_clear(&tmp1, &tmp2, &tmp3));
-	if (!bignum_grow(&tmp2, n))
-		return (do_clear(&tmp1, &tmp2, &tmp3));
-	if (!bignum_exp(&tmp3, &tmp1, &tmp2))
-		return (do_clear(&tmp1, &tmp2, &tmp3));
-	if (!bignum_div(r, a, &tmp3))
-		return (do_clear(&tmp1, &tmp2, &tmp3));
-	do_clear(&tmp1, &tmp2, &tmp3);
-	bignum_trunc(r);
+	return (0);
+}
+
+static int		init(t_bignum *tmp, t_bignum *a, uint32_t n, uint32_t *s8)
+{
+	uint32_t	len;
+
+	*s8 = sizeof(*tmp->data) * 8;
+	len = (bignum_num_bits(a) - n + *s8 - 1) / *s8;
+	bignum_init(tmp);
+	if (!bignum_reserve(tmp, len))
+		return (0);
+	tmp->len = len;
+	return (1);
+}
+
+static void		do_shift(t_bignum_word *tmp, t_bignum *a, uint32_t *next, uint32_t s8)
+{
+	uint32_t	nms8;
+
+	nms8 = *next % s8;
+	*tmp = (a->data[*next / s8]
+			& ((t_bignum_word)(-1) << nms8)) >> nms8;
+	if ((*next += s8) >= a->len * s8)
+		return ;
+	nms8 = *next % s8;
+	*tmp |= (a->data[*next / s8]
+			& ((t_bignum_word)(-1) >> (s8 - nms8))) << (s8 - nms8);
+}
+
+int			bignum_rshift_op(t_bignum *r, t_bignum *a, uint32_t n)
+{
+	t_bignum	tmp;
+	uint32_t	next;
+	uint32_t	i;
+	uint32_t	s8;
+
+	if (test_simple(r, a, n))
+		return (1);
+	if (!init(&tmp, a, n, &s8))
+		return (0);
+	i = -1;
+	while (++i < tmp.len)
+	{
+		next = i * s8 + n;
+		do_shift(&tmp.data[i], a, &next, s8);
+	}
+	bignum_move(r, &tmp);
 	return (1);
 }
